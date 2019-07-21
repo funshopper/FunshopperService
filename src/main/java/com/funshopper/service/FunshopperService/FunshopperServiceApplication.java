c0b863792166363e66df02c5622a4593529faa50
Deepak RP <deepu1002@gmail.com>
2019-07-20 23:37:24 -0400

    First commit

---
+++ src/main/java/com/funshopper/service/FunshopperService/FunshopperServiceApplication.java
@@ -0,0 +1,13 @@
+package com.funshopper.service.FunshopperService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FunshopperServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FunshopperServiceApplication.class, args);
+	}
+
+}
